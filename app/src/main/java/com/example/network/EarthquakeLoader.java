487bff6de165a0bd2aecf7dcb80101449f033df1
govardhankanki <Git@2297>
2020-08-30 09:54:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/network/EarthquakeLoader.java
@@ -0,0 +1,37 @@
+package com.example.network;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.util.Log;
+
+import java.util.List;
+
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    private String mUrl;
+
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        // TODO: Finish implementing this constructor
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<Earthquake> loadInBackground() {
+        // TODO: Implement this method
+        if (mUrl == null) {
+            return null;
+        }
+        Log.i(LOG_TAG,"fetching earthQuake data");
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(mUrl);
+        return earthquakes;
+    }
+}
